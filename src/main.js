183a1f2418f212425d6c91ade1db30d5814e3b18
Mayckol Rezende <mayckoll@live.com>
2021-04-01 17:31:24 -0300

    add guard on routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
 
-createApp(App).use(router, store).mount('#app')
+router.beforeEach((to, from, next) => {
+  const loggedIn = store.getters["authStore/loggedIn"];
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!loggedIn) {
+      next({
+        name: "Login"
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (loggedIn) {
+      next({
+        name: "Main"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+createApp(App)
+  .use(router, store)
+  .mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Auth from "../components/Auth.vue";
+import Home from "@/views/Home.vue";
+import Register from "@/components/Auth/Register.vue";
+import Login from "@/components/Auth/Login.vue";
+import Logout from "@/components/Auth/Logout.vue";
+import Main from "@/views/Main.vue";
 
 const routes = [
   {
@@ -9,18 +12,42 @@ const routes = [
     component: Home
   },
   {
-    path: '/auth',
-    name: 'Auth',
-    component: Auth
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      requiresVisitor: true,
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    meta: {
+      requiresVisitor: true,
+    }
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: Logout
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main,
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
